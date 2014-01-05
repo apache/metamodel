5ce666fa3b601ddee57e24a7bfb86d49046cd582
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2014-01-05 20:55:39 +0100

    Added test case for Excel data contexts.

--- spring/src/test/java/org/apache/metamodel/spring/ExcelSpecifiedConfigurationFromFilenameTest.java
+++ spring/src/test/java/org/apache/metamodel/spring/ExcelSpecifiedConfigurationFromFilenameTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 
 import org.apache.metamodel.DataContext;
-import org.apache.metamodel.csv.CsvDataContext;
+import org.apache.metamodel.excel.ExcelDataContext;
 import org.apache.metamodel.util.FileResource;
 import org.apache.metamodel.util.Resource;
 import org.junit.Assert;
@@ -16,25 +16,26 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:examples/csv-using-filename.xml")
-public class CsvSpecifiedConfigurationFromFilenameTest {
+@ContextConfiguration("classpath:examples/excel-using-filename.xml")
+public class ExcelSpecifiedConfigurationFromFilenameTest {
 
     @Autowired
     private DataContext dataContext;
 
     @Test
     public void testReadDataContext() {
-        assertEquals(CsvDataContext.class, dataContext.getClass());
+        assertEquals(ExcelDataContext.class, dataContext.getClass());
 
-        CsvDataContext csv = (CsvDataContext) dataContext;
-        Resource resource = csv.getResource();
+        ExcelDataContext excel = (ExcelDataContext) dataContext;
+        Resource resource = excel.getResource();
 
         assertEquals(FileResource.class, resource.getClass());
 
-        assertEquals("example.csv", resource.getName());
+        assertEquals("example.xlsx", resource.getName());
 
-        assertEquals("[\"foo\", bar]", Arrays.toString(csv.getDefaultSchema().getTable(0).getColumnNames()));
+        assertEquals("[hello, world]", Arrays.toString(excel.getDefaultSchema().getTable(0).getColumnNames()));
 
-        Assert.assertFalse(csv.getConfiguration().isMultilineValues());
+        Assert.assertTrue(excel.getConfiguration().isSkipEmptyLines());
+        Assert.assertTrue(excel.getConfiguration().isSkipEmptyColumns());
     }
 }
