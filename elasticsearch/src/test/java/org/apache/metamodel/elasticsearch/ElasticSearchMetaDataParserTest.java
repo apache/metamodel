b7f609ac22f2a53dfe1b87eab02d2ec84a5ff7fe
unknown <mdelaet@MDELAETT.gsdm.com>
2015-05-14 13:20:41 -0500

    changed ElasticSearchDataContext.java detectTable to iterate through the mappingMetaData and only parse the properties element
    changed ElasticSearchMetaDataParser.java parse to split on "}," so that it
    keeps the full description of a field with the field name, and adjusted
    getMetaDataFieldTupeFromMetaDataField to handle the presence of extra
    field descriptors and the edge case of the last field which will end with
    a "}"
    
    changed ElasticSearchMetaDataParserTest.java to test the case when a field
    has an extra descriptor by adding the index=not_analyzed tag to the
    message field in the metaDataInfo string

--- elasticsearch/src/test/java/org/apache/metamodel/elasticsearch/ElasticSearchMetaDataParserTest.java
+++ elasticsearch/src/test/java/org/apache/metamodel/elasticsearch/ElasticSearchMetaDataParserTest.java
@@ -24,7 +24,7 @@ import org.apache.metamodel.schema.ColumnType;
 public class ElasticSearchMetaDataParserTest extends TestCase {
 
     public void testParseMetadataInfo() throws Exception {
-        String metaDataInfo = "{message={type=long}, " +
+        String metaDataInfo = "{message={type=long,index=not_analyzed}, " +
                 "postDate={type=date, format=dateOptionalTime}, " +
                 "anotherDate={type=date, format=dateOptionalTime}, " +
                 "user={type=string}, " +
