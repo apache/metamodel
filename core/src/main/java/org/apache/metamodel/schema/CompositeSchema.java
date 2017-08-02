725e2102b439bbd323a44d1f6e2efb2720371b86
Jörg Unbehauen <joerg@unbehauen.net>
2017-08-02 15:05:33 +0200

    previously forgotten

--- core/src/main/java/org/apache/metamodel/schema/CompositeSchema.java
+++ core/src/main/java/org/apache/metamodel/schema/CompositeSchema.java
@@ -25,7 +25,6 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.metamodel.DataContext;
-import org.apache.metamodel.util.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
