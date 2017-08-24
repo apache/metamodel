65752f520640c7f8ba9c128f281568086c050a78
Dennis Du Krøger <losd@apache.org>
2017-08-24 14:36:15 +0200

    No unneeded OFFSET, fix and improves rewriter tests

--- jdbc/src/main/java/org/apache/metamodel/jdbc/dialects/OffsetFetchQueryRewriter.java
+++ jdbc/src/main/java/org/apache/metamodel/jdbc/dialects/OffsetFetchQueryRewriter.java
@@ -1,20 +1,14 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.apache.metamodel.jdbc.dialects;
 
@@ -29,19 +23,20 @@ public abstract class OffsetFetchQueryRewriter extends DefaultQueryRewriter {
 
     private final String _databaseProductName;
     private final int _databaseSupportedVersion;
-    private final boolean _fetchNeedsOrderBy;
+    private final boolean _fetchNeedsOffsetAndOrderBy;
 
     public OffsetFetchQueryRewriter(final JdbcDataContext dataContext, final int minSupportedVersion,
             final boolean fetchNeedsOrderBy) {
         super(dataContext);
         _databaseProductName = dataContext.getDatabaseProductName();
         _databaseSupportedVersion = minSupportedVersion;
-        _fetchNeedsOrderBy = fetchNeedsOrderBy;
+        _fetchNeedsOffsetAndOrderBy = fetchNeedsOrderBy;
     }
 
     @Override
     public boolean isFirstRowSupported(final Query query) {
-        return isSupportedVersion(_databaseProductName, _databaseSupportedVersion) && !query.getOrderByClause().isEmpty();
+        return isSupportedVersion(_databaseProductName, _databaseSupportedVersion) && !query.getOrderByClause()
+                .isEmpty();
     }
 
     @Override
@@ -51,7 +46,7 @@ public abstract class OffsetFetchQueryRewriter extends DefaultQueryRewriter {
 
     /**
      * {@inheritDoc}
-     * 
+     *
      * If the Max rows and First row property of the query is set, then we
      * will use the database's "OFFSET i ROWS FETCH NEXT j ROWS ONLY" construct.
      */
@@ -60,15 +55,21 @@ public abstract class OffsetFetchQueryRewriter extends DefaultQueryRewriter {
         final boolean hasOrderBy = !query.getOrderByClause().isEmpty();
         String queryString = super.rewriteQuery(query);
 
-        if(isSupportedVersion(_databaseProductName, _databaseSupportedVersion)) {
+        if (isSupportedVersion(_databaseProductName, _databaseSupportedVersion) && (query.getMaxRows() != null
+                || query.getFirstRow() != null)) {
             final Integer maxRows = query.getMaxRows();
-            final int firstRow = query.getFirstRow() != null ? query.getFirstRow() : 1;
+            Integer firstRow = query.getFirstRow();
 
-            if(!_fetchNeedsOrderBy || hasOrderBy) {
-                queryString = queryString + " OFFSET " + (firstRow-1) + " ROWS";
+            if (!_fetchNeedsOffsetAndOrderBy || hasOrderBy) {
+                if (firstRow != null) {
+                    queryString = queryString + " OFFSET " + (firstRow - 1) + " ROWS";
+                } else if (_fetchNeedsOffsetAndOrderBy) {
+                    // TOP should do it.
+                    return queryString;
+                }
 
                 if (maxRows != null) {
-                    queryString = queryString.replaceAll("TOP [0-9]+", "");
+                    queryString = queryString.replaceAll(" TOP [0-9]+", "");
                     queryString += " FETCH NEXT " + maxRows + " ROWS ONLY";
                 }
             }
--- jdbc/src/test/java/org/apache/metamodel/jdbc/dialects/OracleQueryRewriterTest.java
+++ jdbc/src/test/java/org/apache/metamodel/jdbc/dialects/OracleQueryRewriterTest.java
@@ -1,20 +1,14 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.apache.metamodel.jdbc.dialects;
 
@@ -28,99 +22,86 @@ import org.apache.metamodel.query.FilterItem;
 import org.apache.metamodel.query.OperatorType;
 import org.apache.metamodel.query.Query;
 import org.apache.metamodel.query.SelectItem;
+import org.apache.metamodel.schema.MutableColumn;
+import org.apache.metamodel.schema.MutableSchema;
+import org.apache.metamodel.schema.MutableTable;
 import org.easymock.EasyMock;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class OracleQueryRewriterTest {
+    private MutableTable table;
+    private MutableColumn column;
+    private JdbcDataContext mockContext;
+    private IQueryRewriter qr;
 
-    private static final JdbcDataContext mockContext = EasyMock.createMock(JdbcDataContext.class);
+    @Before
+    public void setUp() throws Exception {
+        table = new MutableTable("foo");
+        table.setSchema(new MutableSchema("MY_SCHEMA"));
+        table.setQuote("\"");
 
-    @BeforeClass
-    public static void initMocks() throws SQLException {
+        column = new MutableColumn("bar");
+        column.setQuote("\"");
+        column.setTable(table);
+
+        mockContext = EasyMock.createMock(JdbcDataContext.class);
         setMetaData(DATABASE_PRODUCT_ORACLE, "R12.1.1.1");
+        qr = new OracleQueryRewriter(mockContext);
     }
 
     @Test
     public void testReplaceEmptyStringWithNull() throws Exception {
-        final OracleQueryRewriter rewriter = new OracleQueryRewriter(mockContext);
         final String alias = "alias";
         SelectItem selectItem = new SelectItem("expression", alias);
         final FilterItem filterItem = new FilterItem(selectItem, OperatorType.DIFFERENT_FROM, "");
-        final String rewrittenValue = rewriter.rewriteFilterItem(filterItem);
+        final String rewrittenValue = qr.rewriteFilterItem(filterItem);
         final String expectedValue = alias + " IS NOT NULL";
-        
+
         assertEquals(expectedValue, rewrittenValue);
     }
 
     @Test
     public void testOffsetFetchConstruct() {
-        final OracleQueryRewriter rewriter = new OracleQueryRewriter(mockContext);
         final int offset = 1000;
         final int rows = 100;
-        final String table = "table";
         final String where = "x > 1";
 
-        Query query = new Query();
-        query.from(table).orderBy("id");
-        final String queryWithoutBoth = query.toSql();
-        Assert.assertEquals("Original SQL is not correctly generated.", " FROM table ORDER BY id ASC", queryWithoutBoth);
-        final String queryWithoutBothRewritten = rewriter.rewriteQuery(query);
-        Assert.assertEquals("There shouldn't be OFFSET-FETCH clause.", queryWithoutBoth, queryWithoutBothRewritten);
+        final String offsetClause = " OFFSET " + (offset - 1) + " ROWS";
+        final String fetchClause = " FETCH NEXT " + rows + " ROWS ONLY";
+
+        final Query query = new Query().from(table).select(column);
+        Assert.assertEquals("There shouldn't be OFFSET-FETCH clause.", query.toSql(), qr.rewriteQuery(query));
 
         query.setFirstRow(offset);
-        final String queryWithoutMax = query.toSql();
-        Assert.assertEquals("Original SQL is not correctly generated.", " FROM table ORDER BY id ASC", queryWithoutMax);
-        final String queryWithoutMaxRewritten = rewriter.rewriteQuery(query);
-        Assert.assertEquals("There shouldn't be OFFSET-FETCH clause.", queryWithoutMax, queryWithoutMaxRewritten);
+        Assert.assertEquals("Wrong or missing OFFSET clause.", query.toSql() + offsetClause, qr.rewriteQuery(query));
 
-        query.setMaxRows(rows).where(where);
-        final String originalQuery = query.toSql();
-        Assert.assertEquals("Original SQL is not correctly generated.", " FROM table WHERE x > 1 ORDER BY id ASC", originalQuery);
+        query.setMaxRows(rows);
+        Assert.assertEquals("Wrong or missing OFFSET and FETCH clauses.", query.toSql() + offsetClause + fetchClause,
+                qr.rewriteQuery(query));
 
-        String rewrittenQuery = rewriter.rewriteQuery(query);
-        final String offsetFetchClause = " OFFSET " + (offset-1) + " ROWS FETCH NEXT " + rows + " ROWS ONLY";
-        Assert.assertEquals("Not correctly generated Offset Fetch clouse.", originalQuery + offsetFetchClause, rewrittenQuery);
+        query.setFirstRow(null);
+        Assert.assertEquals("Wrong or missing FETCH clause.", query.toSql() + fetchClause, qr.rewriteQuery(query));
     }
 
     @Test
     public void testOffsetFetchVersionCheck() throws SQLException {
         setMetaData(DATABASE_PRODUCT_ORACLE, "10.1.1.1");
 
-        final int offset = 1000;
-        final int rows = 100;
-        final String table = "table";
-
-        Query query = new Query();
-        query.from(table).setFirstRow(offset).setMaxRows(rows);
-        final String originalQuery = query.toSql();
-        Assert.assertEquals("Original SQL is not correctly generated.", " FROM table", originalQuery);
-
-        final OracleQueryRewriter rewriter = new OracleQueryRewriter(mockContext);
-        String rewrittenQuery = rewriter.rewriteQuery(query);
-        Assert.assertEquals("The query shouldn't be rewritten.", originalQuery, rewrittenQuery);
+        Query query = new Query().from(table).select(column).setFirstRow(1000).setMaxRows(100);
+        Assert.assertEquals("The query shouldn't be rewritten.", query.toSql(), qr.rewriteQuery(query));
     }
 
     @Test
     public void testOffsetFetchVersionIsNull() throws SQLException {
         setMetaData(DATABASE_PRODUCT_ORACLE, null);
 
-        final int offset = 1000;
-        final int rows = 100;
-        final String table = "table";
-
-        Query query = new Query();
-        query.from(table).setFirstRow(offset).setMaxRows(rows);
-        final String originalQuery = query.toSql();
-        Assert.assertEquals("Original SQL is not correctly generated.", " FROM table", originalQuery);
-
-        final OracleQueryRewriter rewriter = new OracleQueryRewriter(mockContext);
-        String rewrittenQuery = rewriter.rewriteQuery(query);
-        Assert.assertEquals("The query shouldn't be rewritten.", originalQuery, rewrittenQuery);
+        Query query = new Query().from(table).select(column).setFirstRow(1000).setMaxRows(100);
+        Assert.assertEquals("The query shouldn't be rewritten.", query.toSql(), qr.rewriteQuery(query));
     }
 
-    private static void setMetaData(String productName, String version) throws SQLException {
+    private void setMetaData(String productName, String version) throws SQLException {
         EasyMock.reset(mockContext);
 
         EasyMock.expect(mockContext.getDatabaseProductName()).andReturn(productName).anyTimes();
