fce0693d6ba3a5ccab34d35607d69f3f5dc6038c
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2017-10-18 14:11:27 +0200

    METAMODEL-86: Removed dep. on AbstractDataContext intricasies

--- jdbc/src/main/java/org/apache/metamodel/jdbc/JdbcUpdateCallbackDataContext.java
+++ jdbc/src/main/java/org/apache/metamodel/jdbc/JdbcUpdateCallbackDataContext.java
@@ -21,18 +21,22 @@ package org.apache.metamodel.jdbc;
 import java.sql.Connection;
 import java.util.List;
 
-import org.apache.metamodel.AbstractDataContext;
 import org.apache.metamodel.DataContext;
 import org.apache.metamodel.MetaModelException;
 import org.apache.metamodel.data.DataSet;
+import org.apache.metamodel.query.CompiledQuery;
 import org.apache.metamodel.query.Query;
+import org.apache.metamodel.query.builder.InitFromBuilder;
+import org.apache.metamodel.query.builder.InitFromBuilderImpl;
+import org.apache.metamodel.schema.Column;
 import org.apache.metamodel.schema.Schema;
+import org.apache.metamodel.schema.Table;
 
 /**
  * A delegate {@link DataContext} which wraps the {@link JdbcDataContext} for schema traversal, but uses a fixed
  * connection to execute any queries
  */
-final class JdbcUpdateCallbackDataContext extends AbstractDataContext {
+final class JdbcUpdateCallbackDataContext implements DataContext {
 
     private final JdbcDataContext delegate;
     private final Connection connection;
@@ -44,21 +48,73 @@ final class JdbcUpdateCallbackDataContext extends AbstractDataContext {
 
     @Override
     public DataSet executeQuery(Query query) throws MetaModelException {
-        return delegate.executeQuery(connection, query); 
+        // use the overloaded method that defines the query connection
+        return delegate.executeQuery(connection, query);
     }
 
     @Override
-    protected List<String> getSchemaNamesInternal() {
+    public InitFromBuilder query() {
+        // produce an InitFromBuilder that uses _this_ datacontext
+        return new InitFromBuilderImpl(this);
+    }
+
+    @Override
+    public DataContext refreshSchemas() {
+        delegate.refreshSchemas();
+        return this;
+    }
+
+    @Override
+    public DataSet executeQuery(String queryString) throws MetaModelException {
+        final Query query = parseQuery(queryString);
+        return executeQuery(query);
+    }
+
+    @Override
+    public DataSet executeQuery(CompiledQuery compiledQuery, Object... values) {
+        // it would be better to reuse the connection here, but JDBC compiled queries (prepared statements) are tied to
+        // the connections with which they where made, so the connection cannot be replaced.
+        return delegate.executeQuery(compiledQuery, values);
+    }
+
+    @Override
+    public List<Schema> getSchemas() throws MetaModelException {
+        return delegate.getSchemas();
+    }
+
+    @Override
+    public List<String> getSchemaNames() throws MetaModelException {
         return delegate.getSchemaNames();
     }
 
     @Override
-    protected String getDefaultSchemaName() {
-        return delegate.getDefaultSchemaName();
+    public Schema getDefaultSchema() throws MetaModelException {
+        return delegate.getDefaultSchema();
     }
 
     @Override
-    protected Schema getSchemaByNameInternal(String name) {
-        return delegate.getSchemaByNameInternal(name);
+    public Schema getSchemaByName(String name) throws MetaModelException {
+        return delegate.getSchemaByName(name);
     }
+
+    @Override
+    public Query parseQuery(String queryString) throws MetaModelException {
+        return delegate.parseQuery(queryString);
+    }
+
+    @Override
+    public CompiledQuery compileQuery(Query query) throws MetaModelException {
+        return delegate.compileQuery(query);
+    }
+
+    @Override
+    public Column getColumnByQualifiedLabel(String columnName) {
+        return delegate.getColumnByQualifiedLabel(columnName);
+    }
+
+    @Override
+    public Table getTableByQualifiedLabel(String tableName) {
+        return delegate.getTableByQualifiedLabel(tableName);
+    }
+
 }
