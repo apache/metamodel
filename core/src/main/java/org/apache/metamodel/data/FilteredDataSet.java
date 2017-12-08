fc3a1a86a7579e7bb399f68b6c7242184d02ec2b
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2017-12-07 22:14:27 -0800

    Fixed bad indentation

--- core/src/main/java/org/apache/metamodel/data/FilteredDataSet.java
+++ core/src/main/java/org/apache/metamodel/data/FilteredDataSet.java
@@ -25,52 +25,52 @@ import java.util.Collection;
  */
 public final class FilteredDataSet extends AbstractDataSet implements WrappingDataSet {
 
-	private final DataSet _dataSet;
-	private final IRowFilter[] _filters;
-	private Row _row;
-	
-	public FilteredDataSet(DataSet dataSet, Collection<? extends IRowFilter> filterItems) {
-	    this(dataSet, filterItems.stream().toArray(IRowFilter[]::new));
-	}
+    private final DataSet _dataSet;
+    private final IRowFilter[] _filters;
+    private Row _row;
 
-	public FilteredDataSet(DataSet dataSet, IRowFilter... filters) {
-	    super(dataSet);
-		_dataSet = dataSet;
-		_filters = filters;
-	}
+    public FilteredDataSet(DataSet dataSet, Collection<? extends IRowFilter> filterItems) {
+        this(dataSet, filterItems.stream().toArray(IRowFilter[]::new));
+    }
+
+    public FilteredDataSet(DataSet dataSet, IRowFilter... filters) {
+        super(dataSet);
+        _dataSet = dataSet;
+        _filters = filters;
+    }
 
     @Override
-	public void close() {
-		super.close();
-		_dataSet.close();
-	}
-	
-	@Override
-	public DataSet getWrappedDataSet() {
-	    return _dataSet;
-	}
+    public void close() {
+        super.close();
+        _dataSet.close();
+    }
 
-	@Override
-	public boolean next() {
-		boolean next = false;
-		while (_dataSet.next()) {
-			Row row = _dataSet.getRow();
-			for (IRowFilter filter : _filters) {
-				next = filter.accept(row);
-				if (!next) {
-					break;
-				}
-			}
-			if (next) {
-				_row = row;
-				break;
-			}
-		}
-		return next;
-	}
+    @Override
+    public DataSet getWrappedDataSet() {
+        return _dataSet;
+    }
 
-	@Override
-	public Row getRow() {
-		return _row;
-	}
+    @Override
+    public boolean next() {
+        boolean next = false;
+        while (_dataSet.next()) {
+            Row row = _dataSet.getRow();
+            for (IRowFilter filter : _filters) {
+                next = filter.accept(row);
+                if (!next) {
+                    break;
+                }
+            }
+            if (next) {
+                _row = row;
+                break;
+            }
+        }
+        return next;
+    }
+
+    @Override
+    public Row getRow() {
+        return _row;
+    }
 }
