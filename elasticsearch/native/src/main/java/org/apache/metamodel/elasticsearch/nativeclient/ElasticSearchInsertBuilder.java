9002155e4feee18a93ef72087e63aa6077fb59d6
Arjan Seijkens <Arjan.Seijkens@humaninference.com>
2018-01-23 14:54:52 +0100

    Reverted some more out of context code (style) changes.

--- elasticsearch/native/src/main/java/org/apache/metamodel/elasticsearch/nativeclient/ElasticSearchInsertBuilder.java
+++ elasticsearch/native/src/main/java/org/apache/metamodel/elasticsearch/nativeclient/ElasticSearchInsertBuilder.java
@@ -1,14 +1,20 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE
- * file distributed with this work for additional information regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.metamodel.elasticsearch.nativeclient;
 
@@ -49,7 +55,7 @@ final class ElasticSearchInsertBuilder extends AbstractRowInsertionBuilder<Elast
         final Object[] values = getValues();
         for (int i = 0; i < columns.length; i++) {
             if (isSet(columns[i])) {
-                final String  name = columns[i].getName();
+                final String name = columns[i].getName();
                 final Object value = values[i];
                 if (ElasticSearchUtils.FIELD_ID.equals(name)) {
                     if (value != null) {
@@ -66,7 +72,7 @@ final class ElasticSearchInsertBuilder extends AbstractRowInsertionBuilder<Elast
         requestBuilder.setSource(valueMap);
 
         final IndexResponse result = requestBuilder.execute().actionGet();
-
+        
         logger.debug("Inserted document: id={}", result.getId());
 
         client.admin().indices().prepareRefresh(indexName).execute().actionGet();
