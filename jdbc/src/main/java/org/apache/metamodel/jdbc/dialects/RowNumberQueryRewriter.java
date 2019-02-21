489d3a02464e04e92a3bc42909a58eaa8822e491
李小保 <lixiaobao@mininglamp.com>
2019-02-21 08:04:07 +0800

    update license info

--- jdbc/src/main/java/org/apache/metamodel/jdbc/dialects/RowNumberQueryRewriter.java
+++ jdbc/src/main/java/org/apache/metamodel/jdbc/dialects/RowNumberQueryRewriter.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.metamodel.jdbc.dialects;
 
 import java.util.List;
@@ -7,8 +25,8 @@ import org.apache.metamodel.query.Query;
 import org.apache.metamodel.query.SelectItem;
 
 /**
- * @author lixiaobao
- * @create 2019-02-01 12:01 AM
+ * Query rewriter for databases that support RowNumber keywords for max
+ * rows and first row properties.
  **/
 
 public class RowNumberQueryRewriter extends DefaultQueryRewriter {
