1d5006c52de8774a9135a6a180af5dc8e5fe22b2
Kasper Sørensen <i.am.kasper.sorensen@gmail.com>
2019-04-15 04:42:19 -0700

    Improved stability of Salesforce test and exception handling.
    This should avoid failures such as the one that broke the assertion in
    Travis CI recently; https://api.travis-ci.org/v3/job/517589252/log.txt

--- salesforce/src/test/java/org/apache/metamodel/salesforce/SalesforceDataContextTest.java
+++ salesforce/src/test/java/org/apache/metamodel/salesforce/SalesforceDataContextTest.java
@@ -80,9 +80,7 @@ public class SalesforceDataContextTest extends SalesforceTestCase {
             new SalesforceDataContext("foo", "bar", "baz");
             fail("Exception expected");
         } catch (IllegalStateException e) {
-            assertEquals(
-                    "Failed to log in to Salesforce service: INVALID_LOGIN: Invalid username, password, security token; or user locked out.",
-                    e.getMessage());
+            assertTrue(e.getMessage(), e.getMessage().startsWith("Failed to log in to Salesforce service"));
         }
     }
 
